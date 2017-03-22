fba7d9f98ba9a15068a1d0410947c950e6518932
MaxArt1405 <kondratovich4444@gmail.com>
2017-03-22 16:40:34 +0300

    Homework_1

--- Homework_1/myscript.js
+++ Homework_1/myscript.js
@@ -1,44 +1,49 @@
-var a = prompt("Введите число a")
-var b = prompt("Введите действие")
-var c = prompt("Введите число b")
-switch(b)
+var a = prompt("Введите число a");
+var b = prompt("Введите действие");
+var c = prompt("Введите число b");
+if(Number(a).toString() != "NaN" && Number(c).toString() != "NaN")
 {
-	case("+"):
-		{
-			var d = +a + +c;
-			alert("Результат: " +d);
-		}
-	break;
-	case("-"):
-		{
-			d = a - c;
-			alert("Результат: " +d);
-		}
-	break;
-	case("*"):
-		{
-			d = a * c;
-			alert("Результат: " +d);
-		}
-	break;
-	case("/"):
-		{
-			d = a / c;
-			alert("Результат: " +d);
-		}
-	break;
-	case("^"):
-		{
-			d = Math.pow(a,c);
-			alert("Результат: " +d);
-		}
-	break;
-	case("%"):
-		{
-			d = a % c;
-			alert("Результат: " +d);
-		}
-	break;	
-	default:
-	  	alert("Введено неверное арифметическое действие!")
+	switch(b)
+	{	
+		case("+"):
+			{
+				var d = +a + +c;
+				alert("Результат: " +d);
+			}
+		break;
+		case("-"):
+			{
+				d = a - c;
+				alert("Результат: " +d);
+			}
+		break;
+		case("*"):
+			{
+				d = a * c;
+				alert("Результат: " +d);
+			}
+		break;
+		case("/"):
+			{
+				d = a / c;
+				alert("Результат: " +d);
+			}
+		break;
+		case("^"):
+			{
+				d = Math.pow(a,c);
+				alert("Результат: " +d);
+			}
+		break;
+		case("%"):
+			{
+				d = a % c;
+				alert("Результат: " +d);
+			}
+		break;	
+		default:
+		  	alert("Введено неверное арифметическое действие!")
+	}
 }
+else
+	alert("Введено неверное значениe!");
